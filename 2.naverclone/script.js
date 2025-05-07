dbf8d754150dbdf6709d375fabe539917c3b25bc
heejoon lee <leehj7739@gmail.com>
2025-05-07 16:42:22 +0900

    푸터 완료

---
+++ 2.naverclone/script.js
@@ -0,0 +1,10 @@
+/* top buttons */
+
+document.getElementById('topPayButton').addEventListener('click', function() {
+    window.location.href = 'https://order.pay.naver.com/home';
+});
+
+document.getElementById('topShoppingButton').addEventListener('click', function() {
+    window.location.href = 'https://shopping.naver.com/cart';
+});
+
